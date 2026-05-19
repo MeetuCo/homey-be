99cc0989d9a09675a9440c25f69f1b34566a8d82
Dotu <dallasktucker@gmail.com>
2026-05-19 17:13:25 -0400

    Initialize Project

---
+++ src/main/java/dev/meetuco/homey_be/HomeyBeApplication.java
@@ -0,0 +1,13 @@
+package dev.meetuco.homey_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeyBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeyBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/meetuco/homey_be/HomeyBeApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.meetuco.homey_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeyBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
